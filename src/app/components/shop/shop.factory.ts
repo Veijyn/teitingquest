932ba8d240c30aa29322e1bd49c7145e91f86959
Sven Borheier <Sven-Erik.Borheier@curacon.de>
2025-07-11 23:37:07 +0200

    v0.1 Fix Potion als Buff interpretiert Rücknahme

--- src/app/components/shop/shop.factory.ts
+++ src/app/components/shop/shop.factory.ts
@@ -123,7 +123,7 @@ export function createShopItems(): ShopItem[] {
       name: 'Potion',
       description: 'Heilt 10 HP (Cooldown 5min)\n1 Glas Bier',
       icon: 'health-potion.svg',
-      type: 'buff',
+      type: 'consumable',
       price: 5,
       bonusStats: { hp: 10 },
       acquiredAt: now,
